09f1b888d9f7157c80e12c856093913213309f0c
Daniel Pepper <pepper.daniel@gmail.com>
2024-10-25 05:13:33 +0700

    postinstall

--- Formula/dpep-common.rb
+++ Formula/dpep-common.rb
@@ -1,11 +1,12 @@
 class DpepCommon < Formula
   desc "Common utilities"
   homepage "https://github.com/dpep/homebrew-dpep/blob/main/README.md"
-  url "https://github.com/dpep/homebrew-dpep.git", branch: "main"
+  url "https://github.com/dpep/homebrew-dpep.git"
   version "0.0.1"
   license "MIT"
 
   %w[
+    bash
     direnv
     ffmpeg
     gcc
@@ -16,10 +17,13 @@ class DpepCommon < Formula
     jemalloc
     jq
     mysql-client
+    pipenv
     postgresql@14
+    rbenv
     readline
     redis
     ripgrep
+    ruby
     rsync
     sqlite
     tree
@@ -28,4 +32,29 @@ class DpepCommon < Formula
   ].each do |dependency|
     depends_on dependency
   end
+
+  GEMS = %w[
+    bundler
+    ruby-lsp
+    ruby-lsp-rspec
+    irbrc
+    rekey
+    rspec
+  ]
+
+  CASKS = %w[
+    jumpcut
+    visual-studio-code
+  ]
+
+  def post_install
+    puts "Next steps:\n\n"
+
+    puts "brew install --cask #{CASKS.join(" ")}"
+    puts
+
+    puts "gem install #{GEMS.join(" ")}"
+    puts
+  end
+
 end
